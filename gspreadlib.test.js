a94d9872352295675866cae660e7f525e0f5307b
Federico Rota <federico.rota01@gmail.com>
2017-11-26 22:07:08 +0100

    External accessToken in test

--- gspreadlib.test.js
+++ gspreadlib.test.js
@@ -1,6 +1,7 @@
+const accessToken = require('./accessToken');
 const GSpread = require('./gspreadlib');
 
 test('gspread is not null', () => {
-	var helper = new GSpread('123');
+	var helper = new GSpread(accessToken);
   expect(helper).not.toBeNull();
 });
