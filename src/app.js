903f3e0b550298076d6efc291ac8dc3b69d71193
gitfer <federico.rota01@gmail.com>
2018-01-28 00:58:49 +0100

    Prettiefied code

--- src/app.js
+++ src/app.js
@@ -7,33 +7,35 @@ var bodyParser = require('body-parser');
 var expresshandlebars = require('express-handlebars');
 
 var routes = require('./routes');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join('./', 'views'));
-app.engine('handlebars', expresshandlebars({
-  layoutsDir: 'views',
-  defaultLayout: 'layout'
-}));
+app.engine(
+  'handlebars',
+  expresshandlebars({
+    layoutsDir: 'views',
+    defaultLayout: 'layout'
+  })
+);
 app.set('view engine', 'handlebars');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.status(err.status || 500);
   var data = {
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 var getValues = require('../src/gspreadlib');
 var spreadsheetId = process.env.SPREADSHEETID;
-var clientSecretFile = { 
-	installed: {
-		client_secret: process.env.CLIENTSECRET,
-		client_id: process.env.CLIENT_ID,
-		redirect_uris: [process.env.REDIRECT_URI]
-	}
+var clientSecretFile = {
+    installed: {
+        client_secret: process.env.CLIENTSECRET,
+        client_id: process.env.CLIENT_ID,
+        redirect_uris: [process.env.REDIRECT_URI]
+    }
 };
 var valuesFound = getValues(clientSecretFile, spreadsheetId);
-console.log("values: ", valuesFound);
+console.log('values: ', valuesFound);
